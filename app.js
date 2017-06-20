586c0b6a5ac06c189dab97bd042e23dd2e94b037
Landon West <landonwest5@gmail.com>
2017-06-19 22:21:23 -0700

    use twitter package to query api and favorite results of searched hashtag

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-require('dotenv').config();
-const twitter = require('twitter');
+const Twitter = require('twitter');
 const config = require('./config.js');
 
 const T = new Twitter(config);
+
+// Set up Twitter search parameters
+const params = {
+  q: '#nodejs',
+  count: 1,
+  result_type: 'recent',
+  lang: 'en'
+};
+
+T.get('search/tweets', params, function(err, data, response) {
+  if (!err) {
+    data.statuses.forEach(tweet => {
+      // get the tweet id from the returned data
+      let id = { id: tweet.id_str };
+
+      T.post('favorites/create', id, function(err, response) {
+        if (!err) {
+          let username = response.user.screen_name;
+          let tweetId = response.id_str;
+          console.log('favorited: ', `https://twitter.com/${username}/status/${tweetId}`);
+        } else {
+          console.log(err[0].message);
+        }
+      });
+    });
+  } else {
+    console.log('Twitter api error: ', err);
+  }
+});
